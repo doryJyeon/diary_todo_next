fdb387d7edb5c7af76b632e6a57b9e118823a391
JYeon <doryJYeon@gmail.com>
2024-07-12 20:00:08 +0900

    FIX_todo list window check

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,17 @@
 import { Suspense } from "react";
 import TodoFilter from "./components/Todo/TodoFilter";
 import TodoList from "./components/Todo/TodoList";
-import styles from "./components/Todo/Todo.module.css";
 
 // to-do list
 export default function ToDoList() {
   return (
-    <>
-      <Suspense fallback={<TodoFilter loading={true} />}>
-        <TodoFilter />
-      </Suspense>
-      <Suspense
-        fallback={
-          <ul className={styles.list__wrap}>
-            <li>등록된 할 일이 없습니다.</li>
-          </ul>
-        }
-      >
-        <TodoList />
-      </Suspense>
-    </>
+    <Suspense fallback={<TodoFilter loading={true} />}>
+      {typeof window !== "undefined" && (
+        <>
+          <TodoFilter />
+          <TodoList />
+        </>
+      )}
+    </Suspense>
   );
 }
